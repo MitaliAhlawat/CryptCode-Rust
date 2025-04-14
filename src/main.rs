bb0022ec715bdf4b1a490b8dfc8ead545a016519
Mitali Ahlawat <ahlawatmitali@gmail.com>
2025-04-14 09:20:12 -0500

    Fixed crate versions and added encryption decryption skeleton in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use aes::Aes128;
+use block_modes::{BlockMode, Cbc};
+use block_padding::Pkcs7;
+
+type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+
+const KEY_SIZE: usize = 16;
+const IV_SIZE: usize = 16;
+
+fn encrypt(_plaintext: &str, _key: &[u8]) -> Result<String, &'static str> {
+    // TODO: Implement encryption
+    Err("Encryption not yet implemented.")
 }
+
+fn decrypt(_ciphertext: &str, _key: &[u8]) -> Result<String, &'static str> {
+    // TODO: Implement decryption
+    Err("Decryption not yet implemented.")
+}
+
+fn main() {
+    println!("Encryption and decryption functions initialized.");
+}
+
